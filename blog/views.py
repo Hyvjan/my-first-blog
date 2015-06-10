aeb8fdd9e727d4bd54002db9be7b41d2cae8b014
janne <janne.j.hyvonen@gmail.com>
2015-06-10 22:54:55 +0300

    fiksattu views:stä bugi (else:n sisennykset) ja tehty draft_post_list feature.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
+
 
 # Create your views here.
 def post_list(request):
@@ -28,7 +29,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form':form})
     
 def post_edit(request, pk):
-    post=get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     if request.method=="POST":
         form=PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -37,6 +38,10 @@ def post_edit(request, pk):
             post.published_date=timezone.now()
             post.save()
             return redirect('blog.views.post_detail', pk=post.pk)
-        else:
-            form=PostForm(instance=post)
+    else:
+        form=PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form':form})
+        
+def post_draft_list(request):
+    posts=Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
